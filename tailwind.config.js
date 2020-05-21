55dbec6ee3ad953c8070a3afa16de302d0a0161e
Grant Holle <hollegrant@gmail.com>
2020-05-21 22:37:29 +0800

    Add the project files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [
+    './*.html',
+  ],
+  theme: {
+    fontFamily: {
+      sans: `'Heebo', system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"`
+    }
+  },
+  variants: {},
+  plugins: [],
+}
